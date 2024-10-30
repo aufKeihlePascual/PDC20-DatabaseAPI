0cd42a47b571b826f1a9ade05f231e1576c730e5
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2024-10-30 15:18:53 +0800

    First Commit

---
+++ src/components/MyHome.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MyHome = () => {
+    return (
+        <div class="mb-3">
+            <h1>Welcome to the Home Page</h1>
+            <p>This is the home page of the simple navigation example.</p>
+        </div>
+    );
+};
+
+export default MyHome;
---
+++ src/components/MyProfile.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MyProfile = () => {
+    return (
+        <div class="mb-3">
+            <h1>Your Profile</h1>
+            <p>This is your profile page. You can edit your information here.</p>
+        </div>
+    );
+};
+
+export default MyProfile;
