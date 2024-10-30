2a4d1c65ad53d2150add550984a416820400eb31
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2024-10-30 15:38:50 +0800

    Updated code for new lab activity

---
+++ src/components/Nav.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Nav = () => {
+    return (
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+
+                        < NavLink to="/" className="nav-link" activeclassName="active" area-current="page">Home</NavLink>
+                        < NavLink to="/profile" className="nav-link" activeclassName="active" area-current="page">Profile</NavLink>
+                        < NavLink to="/contact" className="nav-link" activeclassName="active" area-current="page">Contact Us</NavLink>
+                    
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,10 @@ import { createRoot } from 'react-dom/client'
 import './event.css'
 // import UserProfilePage from './Profile/UserProfilePage.jsx'
 import MainEvent from './Events/MainEvent.jsx';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <UserProfilePage/> */}
-
-    <MainEvent/>
+    <App/>
   </StrictMode>
 );
